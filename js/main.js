01d72c02ec6004f2dd8c4fcc2f4d3599a75f3ec3
VitalyFront <122453878+VitalyFront@users.noreply.github.com>
2023-02-20 16:35:32 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.fitted-cars__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+      
+
+    });
+});
